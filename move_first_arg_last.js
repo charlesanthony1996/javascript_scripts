00db46edf6463af08dd601e263bc1812151db345
charlesanthony1996 <charlesanthony296@gmail.com>
2025-02-18 11:31:39 +0100

    flip script

--- move_first_arg_last.js
+++ move_first_arg_last.js
@@ -1,1 +1,19 @@
-const flip = fn => (first, last) => fn(...rest, first)
+const flip = fn => (first, ...rest) => fn(...rest, first)
+
+let a = { name: 'John Smith'}
+let b = {}
+var c = {}
+
+const mergeFrom = flip(Object.assign)
+
+// let mergePerson = mergeFrom.bind(null, a)
+let mergePerson1 = mergeFrom.bind(null, c)
+
+// mergePerson(b)
+mergePerson1(c)
+
+b = {}
+c = {}
+
+console.log(Object.assign(b, a))
+console.log(Object.assign(b, c))
